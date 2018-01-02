fc7a254a683a603e33d36526560f71e46a933171
rhauteki <rafaelhautekiet@gmail.com>
2018-01-02 19:24:31 +0100

    changed e_key -> key parameter

--- helper.py
+++ helper.py
@@ -18,21 +18,21 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from extractor import Extractor
 
 class FeatureSelector(BaseEstimator, TransformerMixin):
-    def __init__(self, e_key=None):
-        self.e_key = e_key
+    def __init__(self, key):
+        self.key = key
 
     def fit(self, x, y=None):
         return self
 
     def transform(self, data):
         """
-        if e_key is set sentences will transformed to
+        if key is part of data then preprocessing is already done
         ex. sentence of POS-tags or sentence of lemma_POS-tags
         """
-        if not self.e_key:
-            return data
+        if self.key in data:
+            return data[self.key]
         else:
-            return list(map(lambda s: Extractor(s)[self.e_key], data))
+            return list(map(lambda s: Extractor(s)[self.key], data.ix[:,0]))
 
 class ArrayCaster(BaseEstimator, TransformerMixin):
     def fit(self, x, y=None):
