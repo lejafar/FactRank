8e5dd812164e69cc9fdd2d3958078a4be2f60279
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2019-04-29 10:11:50 +0200

    update about

--- demo/src/components/About.vue
+++ demo/src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
       <vue-markdown>
-**FactRank** is an online and free-to-use software tool that aims to **automatically distill checkworthy statements** from full-text transcripts **in Dutch**. The current version `{{$api_version}}` - [the demo](#demo) - already does this, and as our tests on Flemish parliament debates show, with good-quality results.
+**FactRank** is an online and free-to-use software tool that aims to **automatically distill checkworthy statements** from full-text transcripts **in Dutch**. The current version `{{$api_version}}` - [the demo](/#/demo) - already does this, and as our tests on Belgian parliament debates show, with good-quality results.
 
 ## The road to Factrank 1.0
 
@@ -11,13 +11,13 @@ In order to be of day-to-day use for the public and fact-checkers in professiona
 
 ## Earlier work
 
-The project was originally created during the course _Knowledge and the Web_ by prof. Bettina Berendt at KU Leuven where our[^1] goal was to come up with a method to help clamping down on **Fake news and the spread of misinformation**. As a starting point, we set out to develop an algorithm that would automatically **identify** interesting claims that are worth checking i.e. **Check-worthy Factual Statements**[^2]. Only when such statements can be reliably identified, can they be sent to the next step in the pipeline that actually tries to falsify the interesting claims.
+The project was originally created during the course _Knowledge and the Web_ by prof. Bettina Berendt at KU Leuven where our[^1] goal was to come up with a method to help clamp down on **Fake news and the spread of misinformation**. As a starting point, we set out to develop an algorithm that would automatically **identify** interesting claims that are worth checking i.e. **Check-worthy Factual Statements**[^2]. Only when such statements can be reliably identified, they can be sent to the next step in the pipeline that actually tries to falsify the interesting claims.
 
 
 [^1]: Back then, it was developped by Brecht Laperre, Ivo merchiers and [Rafael Hautekiet](https://github.com/lejafar)
 [^2]: This terminology is also used by [the Claimbuster-project](http://idir-server2.uta.edu/claimbuster/), a big source of inspiration during the project.
 
-A classifier was trained using on **1800** hand-classified **statements** in order to predict the probability of check-worthiness when provided with a statement. A demonstration of its current ability can be found on the [front page](/#/) in the from of a rank of Dutch statements made during the last Plenary Meeting of the Belgian Federal parliament based on their check-worthiness. To provide you with the ability to play around with input of your own and see how this is interpreted by FactRank, we made a little [demo](/#/demo).
+A classifier was trained using **1800** [hand-classified statements](https://github.com/factrank/FactRank/blob/master/data/sentences_dump_28.12.csv) in order to predict the probability of check-worthiness when provided with a statement. A demonstration of its current ability can be found on the [front page](/#/) in the from of a rank of Dutch statements made during the last Plenary Meeting of the Belgian Federal parliament based on their check-worthiness. To provide you with the ability to play around with input of your own and see how this is interpreted by FactRank, we made a little [demo](/#/demo).
 
 </vue-markdown>
 
