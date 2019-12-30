9a5e359d35adbf5523b65b48c8b07ae3b228b352
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2019-12-30 16:32:00 +0100

    improve ui

--- demo/src/components/ResultsTable.vue
+++ demo/src/components/ResultsTable.vue
@@ -15,7 +15,6 @@
             <blockquote class="blockquote">
                 <footer v-if="data.item.speaker || data.item.source "class="blockquote-footer">
                     <span v-if="data.item.speaker" class="speaker">
-                        <img v-if="flemish(data.item.source.name)" style="vertical-align:middle;max-height: 15px;" src="https://www.vlaamsparlement.be/sites/all/themes/balance_theme/favicon.ico">
                         {{data.item.speaker.country | flag(data.item.source.name) }}
                         {{data.item.speaker.name | cleanName }}
                         <cite v-if="data.item.speaker && data.item.speaker.association" title="Speaker">({{data.item.speaker.association.name}})</cite>
@@ -23,6 +22,7 @@
                     <p v-if="data.item.source" class="info">
                         {{ data.item.source.published_at | formatDate }}
                         <a class="source_type text-secondary" :href="data.item.source.url" target="_blank">
+                        	<img v-if="flemish(data.item.source.name)" style="vertical-align:middle;max-height: 15px;" src="https://www.vlaamsparlement.be/sites/all/themes/balance_theme/favicon.ico">
                             <icon :class="data.item.source.type.toLowerCase()" :name="data.item.source.type | pick_icon" size="xs"/>
                             <icon class="url text-context" name="link" size="xs"/>
                         </a>
@@ -107,7 +107,6 @@ export default {
         },
         flag(country, source) {
             if(country == 'BE'){
-                if(source.includes('Flemish')) return;
                 return '🇧🇪 ';
             }
             return '🇳🇱 ';
