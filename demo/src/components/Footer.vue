cee9a6be1f92fa2ec10485f1069c345b85d0a948
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2020-04-12 14:31:44 +0200

    minor update

--- demo/src/components/Footer.vue
+++ demo/src/components/Footer.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="bg-light">
     <b-container>
+		<p>Made with ❤️ in Borgerhout by Rafael Hautekiet, comissioned by Feitsj VZW</p>
         <vue-markdown>{{ $t('footer.feedback') }}</vue-markdown>
     </b-container>
-    </div>
 </template>
 
 <script>
@@ -14,4 +13,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container {
+	margin-top: 2rem;
+	margin-bottom: 2rem;
+	color: #b4bbc1;
+}
+</style>
+<style>
+.container > div > p > a {
+  color: #1976d2 !important;
+  text-decoration: underline;
+}
 </style>
